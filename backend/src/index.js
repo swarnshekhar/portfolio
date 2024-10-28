c2ea421e18a3fb5b288364c2700a7cb26d06f6bf
swarn <swarnshekhar21j@gmail.com>
2024-10-29 02:07:51 +0530

    hii

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,9 +17,15 @@ const PORT = process.env.PORT || 5000;
 // }));
 
 app.use(cors({
-  origin: ['http://localhost:5173','https://swarnshekhar.github.io', 'https://swarnshekhar.github.io/portfolio'], // Your frontend URLs
+  origin: [
+    'http://localhost:5173', 
+    'http://localhost:4173',  // Add this line
+    'https://swarnshekhar.github.io', 
+    'https://swarnshekhar.github.io/portfolio'
+  ],
   methods: ['POST', 'OPTIONS'], // Allow specific methods
 }));
+
 app.options('/api/contact', cors());
 
 
